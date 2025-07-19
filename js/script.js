8d707dcebb1a0fc7768975251e9d787021064537
corvinyy <cviny072@gmail.com>
2025-07-19 18:37:42 -0300

    feat: add input validation and password size functions

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-function getChartTypes() {
+// function to get character types based on user selection
+function getCharTypes() {
     
     const uppercase = document.querySelector('#uppercase').checked;
     const lowercase = document.querySelector('#lowercase').checked;
     const numbers = document.querySelector('#numbers').checked;
     const symbols = document.querySelector('#symbols').checked;
 
-    const charTypes = [];
+    const charOptions = [];
 
     if (uppercase) {
-        charTypes.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        charOptions.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     }
 
     if (lowercase) {
-        charTypes.push('abcdefghijklmnopqrstuvwxyz');
+        charOptions.push('abcdefghijklmnopqrstuvwxyz');
     }
 
     if (numbers) {
-        charTypes.push('0123456789');
+        charOptions.push('0123456789');
     }
 
     if (symbols) {
-        charTypes.push('!@#$%^&*()_+[]{}|;:,.<>?');
+        charOptions.push('!@#$%^&*()_+[]{}|;:,.<>?');
     }
 
-    return charTypes;
+    return charOptions;
 }
 
+// function to validate input based on selected character types
+function validateInput(charOptions) {
+    const randomIndex = Math.floor(Math.random() * charOptions.length);
+    return charOptions[randomIndex][Math.floor(Math.random() * charOptions[randomIndex].length)];
+}
+
+//function to get the size of the password from user input
+function getSize() {
+    const sizeInput = document.querySelector('#length').value;
+
+    if (sizeInput < 4 || sizeInput > 64 || isNaN(sizeInput)) {
+        alert('Please enter a valid number between 4 and 64.');
+    }
+
+    return sizeInput;
+}
+
+// Event listener for the generate button
 document.querySelector('#generate-btn').addEventListener('click', function() {
-    console.log(getChartTypes());
+    console.log(validateInput(getSize()));
 });
