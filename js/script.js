9e9fdbae8df1e585888b27d2a7da0b3a158f498d
corvinyy <cviny072@gmail.com>
2025-07-19 15:28:07 -0300

    feat: implement character type selection logic for password generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-//test
+function getChartTypes() {
+    
+    const uppercase = document.querySelector('#uppercase').checked;
+    const lowercase = document.querySelector('#lowercase').checked;
+    const numbers = document.querySelector('#numbers').checked;
+    const symbols = document.querySelector('#symbols').checked;
+
+    const charTypes = [];
+
+    if (uppercase) {
+        charTypes.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    }
+
+    if (lowercase) {
+        charTypes.push('abcdefghijklmnopqrstuvwxyz');
+    }
+
+    if (numbers) {
+        charTypes.push('0123456789');
+    }
+
+    if (symbols) {
+        charTypes.push('!@#$%^&*()_+[]{}|;:,.<>?');
+    }
+
+    return charTypes;
+}
+
+document.querySelector('#generate-btn').addEventListener('click', function() {
+    console.log(getChartTypes());
+});
